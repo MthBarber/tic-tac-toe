d83ed1a3f85692e665dd8d27739a537bc3eb834f
MthBarber <mthbarber@gmail.com>
2022-01-01 13:38:20 +0000

    begin making objects

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const gameboard = {
     grid = [1,2,3,4,5,6,7,8,9]
     
 };
+
+const player1 = {
+    addX(){
+        return 
+    }
+}
+
+const player2 = {
+    add0(){
+        return
+    }
+}
